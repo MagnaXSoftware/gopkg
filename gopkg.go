340b79aac198375b4d30e256f49c6c83e16cf03d
t0xicCode <xavier.l@magnax.ca>
2021-02-15 20:03:47 -0500

    Update documentation

--- gopkg.go
+++ gopkg.go
@@ -1,3 +1,20 @@
+// GoPkg implements quick & simple go vanity package import paths.
+//
+// Vanity go package import paths give a cleaner appearance to go projects by separating the source code location from
+// the import path. It also gives flexibility to developers by allowing them to change a project's source code hosting
+// platform without requiring the project to be renamed.
+// Finally, it allows projects hosted on various platforms to be grouped under a common import path.
+//
+// Within a Caddyfile, new go packages are added using the gopkg directive:
+//
+//     gopkg <path> [<vcs>] <uri>
+//
+// The <path> argument corresponds to the path component of the vanity import path, e.g. for "magnax.ca/caddy/gopkg",
+// the path would be "/caddy/gopkg".
+// The <vcs> argument is optional, and defaults to "git". If it is specified, it is used to indicate which version
+// control system is being used to manage the source.
+// The <uri> argument corresponds to the URI/URL of the source code repository. Any format supported by the given VCS
+// and the "go get" tool is can be used, as gopkg does not attempt to validate it.
 package gopkg
 
 import (
@@ -11,7 +28,7 @@ import (
 	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
 )
 
-//DefaultTemplate is the default HTML template used as a response.
+// DefaultTemplate is the default HTML template used as a response.
 const DefaultTemplate = `<html>
 <head>
 <meta name="go-import" content="{{.Host}}{{.Path}} {{.Vcs}} {{.URI}}">
@@ -27,11 +44,28 @@ func init() {
 	httpcaddyfile.RegisterDirective("gopkg", parseCaddyFile)
 }
 
-//Module represents the GoPkg Caddy module.
+// Module implements vanity go package import paths.
+//
+// Vanity go package import paths give a cleaner appearance to go projects by separating the source code location from
+// the import path. It also gives flexibility to developers by allowing them to change a project's source code hosting
+// platform without requiring the project to be renamed.
+// Finally, it allows projects hosted on various platforms to be grouped under a common import path.
 type Module struct {
+	// Path is the HTTP path component of the vanity import path.
+	//
+	// Given a vanity import path of `web.site/package/name`, the path would be `/package/name`.
 	Path string `json:"path"`
-	Vcs  string `json:"vcs,omitempty"`
-	URI  string `json:"uri"`
+
+	// Vcs is the version control system used by the package.
+	//
+	// If empty, the default is `git`.
+	// Valid values include `git`, `hg`, `svn`, `bzr`, `cvs`. Basically, any version control system that go knows how to address.
+	Vcs string `json:"vcs,omitempty"`
+
+	// URI is the URI/URL of the package's source.
+	//
+	// This is where the go tool will go to download the source code.
+	URI string `json:"uri"`
 
 	// Template is the template used when returning a response (instead of redirecting).
 	Template *template.Template
